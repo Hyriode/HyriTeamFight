494d1241a1ed75969adb0af7ac3df38e0d55a288
AstFaster <astfaster@gmail.com>
2023-05-16 18:46:54 +0200

    Intial commit

---
+++ src/main/java/fr/hyriode/teamfight/HyriTeamFight.java
@@ -0,0 +1,21 @@
+package fr.hyriode.teamfight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by AstFaster
+ * on 16/05/2023 at 18:42
+ */
+public class HyriTeamFight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
